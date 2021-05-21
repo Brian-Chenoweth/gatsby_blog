f04829f2c795ac46b935ae3baf0e94445f7b8606
Brian-Chenoweth <Brian.Michael.Chenoweth@gmail.com>
2021-05-20 20:14:49 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby blog",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
